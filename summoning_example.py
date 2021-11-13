b970212df8fe9f87b83083a321e79378a9dd9691
Ortis <ortis@ortis.io>
2021-11-13 09:53:39 +0900

    Full summon

--- summoning_example.py
+++ summoning_example.py
@@ -13,7 +13,11 @@ if __name__ == "__main__":
     rpc_server = 'https://api.harmony.one'
     logger.info("Using RPC server " + rpc_server)
 
-    crystals = summoning.get_user_crystal_ids('0x2E7669F61eA77F02445A015FBdcFe2DE47083E02', rpc_server)
+    #summoning.summon_crystal(hero_id_1, hero_id_2, hero1_tears, hero2_tears, private_key, nonce, gas_price_gwei, 30, rpc_address, logger)
 
+    crystals = summoning.get_user_crystal_ids('0x2E7669F61eA77F02445A015FBdcFe2DE47083E02', rpc_server)
     logger.info("Crystal ids: " + str(crystals))
+
+    #summoning.open_crystal(crystals[0], private_key, nonce, gas_price_gwei, 30, rpc_address, logger)
+
 
